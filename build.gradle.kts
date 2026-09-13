dca9729260b10035c787067472517b60a075248b
subochev <caffeine.mgn@gmail.com>
2026-09-13 23:14:37 +0300

    Configure gpg-agent signatory in build script
    vanniktech.maven.publish's signAllPublications() doesn't read
    signing.gnupg properties on its own; we need to call signing.useGpgCmd()
    explicitly so the standard signing plugin activates a signatory.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("jvm")
     `java-gradle-plugin`
     `maven-publish`
+    signing
     id("org.jetbrains.dokka") version "2.0.0"
     id("com.gradle.plugin-publish") version "2.2.1"
     id("com.vanniktech.maven.publish") version "0.33.0"
@@ -35,6 +36,12 @@ gradlePlugin {
             implementationClass = "pw.binom.kotlin.clang.ClangPlugin"
             description = "Kotlin-Native Clang"
         }
+    }
+}
+
+if (findProperty("signingUseGpg") == "true") {
+    signing {
+        useGpgCmd()
     }
 }
 
